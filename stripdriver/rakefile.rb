3e2afda11bc0ef6290896c60445f55d0a66a9a24
Austin Glaser <austin@boulderes.com>
2020-03-16 02:12:48 -0600

    Remove stripdriver

--- stripdriver/rakefile.rb
+++
@@ -1,6 +0,0 @@
-PROJECT_CEEDLING_ROOT = "modules/ceedling/vendor/ceedling"
-load "#{PROJECT_CEEDLING_ROOT}/lib/ceedling.rb"
-
-Ceedling.load_project ({:config => 'build_ceedling/project.yml'})
-
-task :default => [ 'test:all' ]
